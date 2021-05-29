7d1afa621eba63c75fb415763faaefcdcb15c938
Kenichi Kamiya <kachick1@gmail.com>
2021-05-29 23:13:16 +0900

    Improve own rspec config

--- spec/own_spec.rb
+++ spec/own_spec.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require_relative 'spec_helper'
-
 RSpec.describe RSpec::Matchers::PowerAssertMatchers do
   describe RSpec::Matchers::PowerAssertMatchers::VERSION do
     it do
