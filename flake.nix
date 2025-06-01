6ba9728cf5150d87a3d1714d3c6df2a002a2431b
Kenichi Kamiya <kachick1@gmail.com>
2025-06-01 12:27:09 +0900

    Upgrade flake and devshell to nixos-25.05 or later  era

--- flake.nix
+++ flake.nix
@@ -1,11 +1,6 @@
 {
   inputs = {
-    # Candidate channels
-    #   - https://github.com/kachick/anylang-template/issues/17
-    #   - https://discourse.nixos.org/t/differences-between-nix-channels/13998
-    # How to update the revision
-    #   - `nix flake update --commit-lock-file` # https://nixos.org/manual/nix/stable/command-ref/new-cli/nix3-flake-update.html
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs =
@@ -15,14 +10,10 @@
     }:
     let
       inherit (nixpkgs) lib;
-      # List: https://github.com/NixOS/nixpkgs/blob/nixos-24.05/lib/systems/flake-systems.nix
-      #
-      # https://github.com/NixOS/nixpkgs/blob/475556854559746466df20d74eef189373816b67/flake.nix?plain=1#L11
-      # https://github.com/NixOS/nixpkgs/blob/475556854559746466df20d74eef189373816b67/lib/systems/default.nix?plain=1#L48-L56
       forAllSystems = lib.genAttrs lib.systems.flakeExposed;
     in
     {
-      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixfmt-rfc-style);
+      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixfmt-tree);
       devShells = forAllSystems (
         system:
         let
